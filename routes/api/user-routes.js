a4d2901bbf46a7c094418484bd67108fdcce10ac
StaticCloud <diegomarrs@gmail.com>
2022-02-16 17:43:59 -0800

    user routes for creating users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,17 +2,34 @@ const router = require('express').Router();
 
 const { User } = require('../../models');
 
+// get all users
 router.get('/', async (req, res) => {
     try {
-        const users = User.findAll(
+        const users = await User.findAll(
             {
-                attributes: ['id', 'username']
+                attributes: { exclude: ['password']}
             }
         )
         res.json(users);
     } catch(err) {
         console.log(err);
         res.status(500).json(err);
+    }
+})
+
+// create new user
+router.post('/', async (req, res) => {
+    try {
+        const user = await User.create(
+            {
+                username: req.body.username,
+                password: req.body.password
+            }
+        )
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
     }
 })
 
