e251a7336227b15704f7d4aa0f3d203466d68289
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-19 17:11:52 -0800

    button select highlight

--- public/javascript/ingredients.js
+++ public/javascript/ingredients.js
@@ -1,16 +1,19 @@
 function ingredientButtonHandler(event) {
-filter.push(event.currentTarget.textContent)
+//highlight button
+event.currentTarget.classList.remove('btn-secondary');
+event.currentTarget.classList.add('btn-success');
 
-const recipeHide = document.querySelectorAll('.' + event.currentTarget.textContent)
-console.log(recipeHide)
-
-recipeHide.forEach((recipe) => {
-    recipe.classList.remove("hide")
-    recipe.classList.add("show")
-})
+//reveal recipes
+  const recipeShow = document.querySelectorAll(
+    "." + event.currentTarget.textContent
+  );
+  recipeShow.forEach((recipe) => {
+    recipe.classList.remove("hide");
+    recipe.classList.add("show");
+  });
 }
 
 //attach event listener to each button
 document.querySelectorAll("#ingredients button").forEach((button) => {
-  button.addEventListener("click", (ingredientButtonHandler));
-})
+  button.addEventListener("click", ingredientButtonHandler);
+});
