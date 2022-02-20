817148ca490f9c249a528aec7aaf47b29c96f163
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-19 17:15:36 -0800

    toggle hide/show

--- public/javascript/ingredients.js
+++ public/javascript/ingredients.js
@@ -1,15 +1,26 @@
 function ingredientButtonHandler(event) {
-//highlight button
-event.currentTarget.classList.remove('btn-secondary');
-event.currentTarget.classList.add('btn-success');
+  //highlight button
+  if (event.currentTarget.classList.contains("btn-secondary")) {
+    event.currentTarget.classList.remove("btn-secondary");
+    event.currentTarget.classList.add("btn-success");
+  } else {
+    event.currentTarget.classList.remove("btn-success");
+    event.currentTarget.classList.add("btn-secondary");
+  }
 
-//reveal recipes
+  //reveal recipes
   const recipeShow = document.querySelectorAll(
     "." + event.currentTarget.textContent
   );
   recipeShow.forEach((recipe) => {
-    recipe.classList.remove("hide");
-    recipe.classList.add("show");
+      if (recipe.classList.contains("hide")) {
+        recipe.classList.remove("hide");
+        recipe.classList.add("show");
+      } else {
+        recipe.classList.remove("show");
+        recipe.classList.add("hide");
+      }
+    
   });
 }
 
