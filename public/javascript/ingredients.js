d38691cf137ffcc363c5a2b29b651052c8c805ab
Zachary Clark <zacclark369@gmail.com>
2022-02-21 17:02:39 -0800

    created new sections into cards, would like to add title, but stumped on how to dynamically create one at the top right now.

--- public/javascript/ingredients.js
+++ public/javascript/ingredients.js
@@ -1,6 +1,47 @@
-const fruitsVeggies = ["apples", "asparagus", "bananas", "broccoli", "bananas", "blueberries", "carrots", "celery", "cauliflower", "cucumbers", "garlic", "ginger","lettuce", "jam", "kale", "lemon", "mango", "mushroom", "olives", "orange", "onion", "pepper", "pineapple", "peach", "potato", "raisins", "raspberries", "spinach", "squash","tomato", "watermelon", "zucchini"]
+const fruitsVeggies = [
+  "apples",
+  "asparagus",
+  "bananas",
+  "broccoli",
+  "bananas",
+  "blueberries",
+  "carrots",
+  "celery",
+  "cauliflower",
+  "cucumbers",
+  "garlic",
+  "ginger",
+  "lettuce",
+  "jam",
+  "kale",
+  "lemon",
+  "mango",
+  "mushroom",
+  "olives",
+  "orange",
+  "onion",
+  "pepper",
+  "pineapple",
+  "peach",
+  "potato",
+  "raisins",
+  "raspberries",
+  "spinach",
+  "squash",
+  "tomato",
+  "watermelon",
+  "zucchini",
+];
 
-const dairy = ["cream", "cheese", "alfredo", "butter", "milk", "mayonnaise", "yogurt"];
+const dairy = [
+  "cream",
+  "cheese",
+  "alfredo",
+  "butter",
+  "milk",
+  "mayonnaise",
+  "yogurt",
+];
 
 const meat = ["beef", "chicken", "fish", "pork"];
 const staples = ["sugar", "flour", "beans", "pasta", "eggs", "oil"];
@@ -10,78 +51,77 @@ const ingredientContainer = document.getElementById("ingredients");
 
 // Fruits & Veggies
 // create container
-var fruitsVeggiesContainer = document.createElement("div")
+var fruitsVeggiesContainer = document.createElement("div");
+fruitsVeggiesContainer.setAttribute("class", "card-content");
 ingredientContainer.appendChild(fruitsVeggiesContainer);
 
-// create buttons
-fruitsVeggies.forEach(ingredient => {
-var button = document.createElement("button");
-button.type = "button";
-button.innerHTML = ingredient;
-button.classList.add('btn');
-button.classList.add('btn-secondary');
-fruitsVeggiesContainer.appendChild(button);
-})
 
+// create buttons
+fruitsVeggies.forEach((ingredient) => {
+  var button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = ingredient;
+  button.classList.add("btn");
+  button.classList.add("btn-secondary");
+  fruitsVeggiesContainer.appendChild(button);
+});
 // dairy
 // create container
-var dairyContainer = document.createElement("div")
+var dairyContainer = document.createElement("div");
+dairyContainer.setAttribute("class", "card-content")
 ingredientContainer.appendChild(dairyContainer);
-
 // create buttons
-dairy.forEach(ingredient => {
-var button = document.createElement("button");
-button.type = "button";
-button.innerHTML = ingredient;
-button.classList.add('btn');
-button.classList.add('btn-secondary');
-dairyContainer.appendChild(button);
-})
-
+dairy.forEach((ingredient) => {
+  var button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = ingredient;
+  button.classList.add("btn");
+  button.classList.add("btn-secondary");
+  dairyContainer.appendChild(button);
+});
 // meat
 // create container
-var meatContainer = document.createElement("div")
+var meatContainer = document.createElement("div");
+meatContainer.setAttribute("class", "card-content")
 ingredientContainer.appendChild(meatContainer);
-
 // create buttons
-meat.forEach(ingredient => {
-var button = document.createElement("button");
-button.type = "button";
-button.innerHTML = ingredient;
-button.classList.add('btn');
-button.classList.add('btn-secondary');
-meatContainer.appendChild(button);
-})
-
+meat.forEach((ingredient) => {
+  var button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = ingredient;
+  button.classList.add("btn");
+  button.classList.add("btn-secondary");
+  meatContainer.appendChild(button);
+});
 // staples
 // create container
-var meatContainer = document.createElement("div")
-ingredientContainer.appendChild(meatContainer);
-
+var staplesContainer = document.createElement("div");
+staplesContainer.setAttribute("class", "card-content")
+ingredientContainer.appendChild(staplesContainer);
 // create buttons
-meat.forEach(ingredient => {
-var button = document.createElement("button");
-button.type = "button";
-button.innerHTML = ingredient;
-button.classList.add('btn');
-button.classList.add('btn-secondary');
-meatContainer.appendChild(button);
-})
-
+staples.forEach((ingredient) => {
+  var button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = ingredient;
+  button.classList.add("btn");
+  button.classList.add("btn-secondary");
+  staplesContainer.appendChild(button);
+});
 // alcohol
 // create container
-var meatContainer = document.createElement("div")
-ingredientContainer.appendChild(meatContainer);
+var alcoholContainer = document.createElement("div");
+alcoholContainer.setAttribute("class", "card-content")
+ingredientContainer.appendChild(alcoholContainer);
 
 // create buttons
-meat.forEach(ingredient => {
-var button = document.createElement("button");
-button.type = "button";
-button.innerHTML = ingredient;
-button.classList.add('btn');
-button.classList.add('btn-secondary');
-meatContainer.appendChild(button);
-})
+alcohol.forEach((ingredient) => {
+  var button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = ingredient;
+  button.classList.add("btn");
+  button.classList.add("btn-secondary");
+  alcoholContainer.appendChild(button);
+});
 
 function ingredientButtonHandler(event) {
   //highlight button
