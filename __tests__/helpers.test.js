e5350e791fe497a419516d03206d5967b393bab5
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-02-22 22:10:55 -0800

    github icon, removed helpers folder

--- __tests__/helpers.test.js
+++
@@ -1,7 +0,0 @@
-const { unique_categories } = require("../utils/helpers");
-
-test("unique_categories() returns an array of unique values", () => {
-  const categories = ["tomato", "tomato", "celery", "cucumber"];
-
-  expect(unique_categories(categories)).toBe(["tomato", "celery", "cucumber"]);
-});
